a0f95f353ba06e6ced8c1fe86bd7f85638116414
0pashich <0pashich@gmail.com>
2022-10-31 16:38:00 +0300

    fix types

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,25 @@
-export enum ProjectStatus {
-  IN_PROGRESS = 'К рассмотрению',
-  APPROVED = 'Согласовано',
-  NOT_APPROVED = 'Отклонено',
-  ARCHIVED = 'В архиве',
-  CLOSED = 'Завершено',
+export enum ProjectState {
   DRAFT = 'Черновик',
+  IN_PROGRESS = 'К рассмотрению',
+  FREEZED = 'Заморожено',
+  ARCHIVED = 'В архиве',
+  DELETED = 'Удалено',
+}
+
+export enum ProjectStatus {
+  UNDEFINED = 'Не определено',
+  ACCEPTED = 'Согласовано',
+  REJECTED = 'Отклонено',
 }
 
 export enum DocumentStatus {
-  IN_PROGRESS = 'К рассмотрению',
-  APPROVED = 'Согласовано',
-  NOT_APPROVED = 'Отклонено',
-  ARCHIVED = 'В архиве',
-  CLOSED = 'Завершено',
+  UNDEFINED = 'Не определено',
+  ACCEPTED = 'Согласовано',
+  REJECTED = 'Отклонено',
 }
 
 export const Enums = {
+  ProjectState,
   ProjectStatus,
   DocumentStatus,
 };
