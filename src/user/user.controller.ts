9e4a56a5ccf672c940c51158cdd0423889935abd
alexPiter <litvn256@gmail.com>
2022-11-08 08:07:15 +0300

    fix getUserId in get avatar

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -48,6 +48,33 @@ export class UserController {
     private filesService: FilesService,
   ) {}
 
+  // ======== get avatar ==========
+  // порядок методов имеет значение! этот дб перед readOne
+  @ApiOperation({
+    description: 'Получить аватар пользователя',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'файл аватара',
+  })
+  @Get('avatar')
+  async getAvatar(@GetUser('_id') userId: ObjectId, @Res() res) {
+    const user = await this.userService.findOne(userId);
+    if (user.avatar) {
+      const file = await this.filesService.findInfo(user.avatar);
+      const filestream = await this.filesService.readStream(user.avatar);
+      if (!filestream) {
+        throw new HttpException(
+          'An error occurred while retrieving file',
+          HttpStatus.EXPECTATION_FAILED,
+        );
+      }
+      res.header('Content-Type', file.contentType);
+      return filestream.pipe(res);
+    } else {
+    }
+  }
+
   // ======== readOne ==========
   @ApiOperation({
     description: 'Получить пользователя по его id',
@@ -104,36 +131,5 @@ export class UserController {
   @UseInterceptors(FileInterceptor('file'))
   setAvatar(@GetUser('_id') userId: string, @UploadedFile() file) {
     return this.userService.setUserAvatar(userId, file.id);
-  }
-
-  // ======== get avatar ==========
-  @ApiOperation({
-    description: 'Получить аватар пользователя по id пользователя',
-  })
-  // @ApiResponse({
-  //   status: 200,
-  //   type: UserDto,
-  // })
-  @ApiParam({
-    name: 'id',
-    description: 'id пользователя',
-  })
-  @Get(':id/avatar')
-  @ApiBadRequestResponse({ type: ApiException })
-  async getAvatar(@Param('id') id: ObjectId, @Res() res) {
-    const user = await this.userService.findOne(id);
-    if (user.avatar) {
-      const file = await this.filesService.findInfo(user.avatar);
-      const filestream = await this.filesService.readStream(user.avatar);
-      if (!filestream) {
-        throw new HttpException(
-          'An error occurred while retrieving file',
-          HttpStatus.EXPECTATION_FAILED,
-        );
-      }
-      res.header('Content-Type', file.contentType);
-      return filestream.pipe(res);
-    } else {
-    }
   }
 }
