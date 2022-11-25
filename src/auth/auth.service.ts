fc8bdc4ab8ed078aa8b9f939c847ff65ecf31b1f
Алексей Литвиновский <76833284+AlexLitvein@users.noreply.github.com>
2022-11-25 14:16:15 +0300

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as argon from 'argon2';
 import { AuthDto, Token } from 'auth/dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { User, UserDocument } from 'user/user.shema';
 
 @Injectable({})
